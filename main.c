cff86af916430d5a1e6ee059551b98c32a521002
JoaquinTaborda <130505200+Avannz@users.noreply.github.com>
2023-10-10 10:24:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,42 @@
 
 /// Estructuras
 
-typedef struct {
-     int legajo;
-     char nombre[20];
-     int edad;
-}persona;
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    int edad;
+} persona;
 
-typedef struct nodoArbol {
-     persona dato;
-     struct nodoArbol * izq;
-     struct nodoArbol * der;
-}nodoArbol;
+typedef struct nodoArbol
+{
+    persona dato;
+    struct nodoArbol * izq;
+    struct nodoArbol * der;
+} nodoArbol;
+
+typedef struct nodo
+{
+    persona dato;
+    struct nodo * sig;
+} nodo;
 
 
 /// Prototipado
 
 nodoArbol* insertar (nodoArbol* arbolito, persona dato);
 nodoArbol* inicNodoArbol();
+nodoArbol* buscarEnArbol(nodoArbol* arbolito, int legajo);
+nodoArbol* buscarEnArbolNombre(nodoArbol* arbolito, char nombre[]);
+nodo* crearNodo (persona aux);
+nodo* inicNodo ();
+nodo* agregarPpio (nodo* lista, nodo* nuevoNodo);
+nodo* cargarAlista (nodoArbol *arbolito, nodo* lista);
+
 void preorder(nodoArbol* arbolito);
+void inorder(nodoArbol *arbolito);
+void recorrerLista (nodo* lista);
+void recorrer(nodoArbol *arbolito);
 
 
 
@@ -29,9 +47,11 @@ void preorder(nodoArbol* arbolito);
 
 int main()
 {
-
+    nodo* lista = inicNodo();
     nodoArbol* arbolito = inicNodoArbol();
     char letra = 's';
+    char nombre[30];
+    int legajo;
     persona dato;
 
     while(letra == 's')
@@ -57,12 +77,31 @@ int main()
 
     }
 
-    preorder(arbolito);
 
-    return 0;
+    printf("MOSTRAR ARBOL: \n");
+    inorder (arbolito);
+
+    printf("\nMOSTRAR LISTA: \n");
+    lista = cargarAlista(arbolito, lista);
+    recorrerLista(lista);
+
+    ///PUNTO CUATRO///
+
+
+    printf("Ingrese el legajo a buscar: ");
+    fflush(stdin);
+    scanf("%i", &legajo);
+
+    nodoArbol* rta = buscarEnArbol(arbolito, legajo);
+    inorder(rta);
+
+    printf("Ingrese el nombre a buscar: ");
+    fflush(stdin);
+    gets(nombre);
+
+    rta = buscarEnArbolNombre(arbolito, nombre);
+    inorder(rta);
 }
-
-
 
 nodoArbol* inicNodoArbol()
 {
@@ -103,7 +142,6 @@ nodoArbol* insertar (nodoArbol* arbolito, persona dato)
 
     }
 
-
     return arbolito;
 }
 
@@ -117,6 +155,123 @@ void preorder(nodoArbol* arbolito)
         preorder(arbolito->izq);
         preorder(arbolito->der);
     }
+}
+
+void inorder(nodoArbol *arbolito)
+{
+
+    if(arbolito)
+    {
+//        inorder(arbolito->izq);
+        printf("/ %d /", arbolito->dato.legajo);
+//        inorder(arbolito->der);
+
+    }
+}
+
+nodo* crearNodo (persona aux)
+{
+
+    nodo* auxN = (nodo*) malloc(sizeof(nodo));
+    auxN->dato = aux;
+    auxN->sig = NULL;
+
+    return auxN;
+}
+
+nodo* inicNodo ()
+{
+
+    return NULL;
+}
+
+nodo* agregarPpio (nodo* lista, nodo* nuevoNodo)
+{
+
+    if(!lista)
+    {
+
+        lista = nuevoNodo;
+    }
+    else
+    {
+
+        nuevoNodo->sig = lista;
+        lista = nuevoNodo;
+    }
+
+
+    return lista;
+}
+
+nodo* cargarAlista (nodoArbol* arbolito, nodo* lista)
+{
+
+    nodo* nuevoNodo = inicNodo();
+
+    if(arbolito)
+    {
+        lista = cargarAlista(arbolito->izq, lista);
+
+        nuevoNodo = crearNodo(arbolito->dato);
+        lista = agregarPpio(lista, nuevoNodo);
+
+        lista = cargarAlista(arbolito->der, lista);
+    }
+
+    return lista;
+}
+
+void recorrerLista (nodo* lista)
+{
+
+    if(lista)
+    {
+        printf(".........................\n");
+        printf("Legajo: %d \n", lista->dato.legajo);
+        printf("Edad: %d \n", lista->dato.edad);
+        printf("Nombre: %s \n\n", lista->dato.nombre);
+
+        lista = lista->sig;
+
+        recorrerLista(lista);
+    }
+}
+
+
+
+nodoArbol* buscarEnArbol(nodoArbol* arbolito, int legajo)
+{
+
+    nodoArbol* rta = arbolito;
+
+    if(arbolito && arbolito->dato.legajo != legajo)
+    {
+        rta= buscarEnArbol(arbolito->izq, legajo);
+        if(!rta)
+            rta= buscarEnArbol(arbolito->der, legajo);
+    }
+
+    return rta;
+}
+
+nodoArbol* buscarEnArbolNombre(nodoArbol* arbolito, char nombre[])
+{
+
+    nodoArbol* rta = inicNodoArbol();
+
+    if(arbolito)
+    {
+        if(strcmp(arbolito->dato.nombre, nombre) == 0)
+            rta = arbolito;
+        else
+        {
+                rta = buscarEnArbolNombre(arbolito, nombre);
+                rta = buscarEnArbolNombre(arbolito, nombre);
+        }
+    }
+
+    return rta;
 }
 
 
